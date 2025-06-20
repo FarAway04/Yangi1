3f077cf05df2eb53c8c6210398131a599583e7bb
FarAway04 <148907334+FarAway04@users.noreply.github.com>
2025-06-20 08:55:35 +0500

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,31 +1,27 @@
+CREATE DATABASE IF NOT EXISTS bot_db;
+USE bot_db;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `movies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_id` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `channels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `admin_id` bigint(20) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `superusers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `super_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `videos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_id` varchar(255) NOT NULL,
-  `caption` text,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
   PRIMARY KEY (`id`)
 );
