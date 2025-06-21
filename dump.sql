2ae4f138c171509f77c21e2a594f9aca2377d345
FarAway04 <148907334+FarAway04@users.noreply.github.com>
2025-06-21 15:18:51 +0500

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,27 +1,17 @@
-CREATE DATABASE IF NOT EXISTS bot_db;
-USE bot_db;
-
-CREATE TABLE IF NOT EXISTS `movies` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `file_id` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS admins (
+    user_id BIGINT PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS `channels` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS superusers (
+    user_id BIGINT PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS channels (
+    username TEXT PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS `superusers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS movies (
+    id SERIAL PRIMARY KEY,
+    file_id TEXT,
+    description TEXT
 );
